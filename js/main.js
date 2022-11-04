9a5feecfb2e116c128be195986125eab2da29b8e
Felix Odete <felix.odette@gmail.com>
2022-11-04 10:30:45 +0300

    Logic to announce winner

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 let dice1 = document.getElementById('dice1');
 let dice2 = document.getElementById('dice2');
-let refreshMe = document.querySelector('.btn')
+let refreshMe = document.querySelector('.btn');
+// const winner = document.getElementById('announcement');
+const cont = document.getElementsByClassName('winner');
 
 dice1.setAttribute('src', '../img/dice1.png');
 dice2.setAttribute('src', '../img/dice2.png');
@@ -9,8 +11,17 @@ const diceFace = ['dice1.png', 'dice2.png', 'dice3.png', 'dice4.png', 'dice5.png
 
 
 refreshMe.addEventListener('click', function() {
-    let randomDice1 = diceFace[Math.floor(Math.random() * diceFace.length)];
-    let randomDice2 = diceFace[Math.floor(Math.random() * diceFace.length)];
-    dice1.setAttribute('src', '../img/' + randomDice1);
-    dice2.setAttribute('src', '../img/' + randomDice2);
+  let randomDice1 = Math.floor(Math.random() * diceFace.length);
+  let randomDice2 = Math.floor(Math.random() * diceFace.length);
+
+  if (randomDice1 > randomDice2) {
+    document.querySelector("h2").innerHTML = 'Player 1 Wins!';
+  } else if (randomDice1 < randomDice2) {
+    document.querySelector("h2").innerHTML = 'Player 2 Wins!';
+  } else {
+    document.querySelector("h2").innerHTML = 'Draw!';
+  }
+
+  dice1.setAttribute('src', '../img/' + diceFace[randomDice1]);
+  dice2.setAttribute('src', '../img/' + diceFace[randomDice2]);
 });
